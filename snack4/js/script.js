a80bec7da6a8f1eabf6e4e158efa74d148a037de
Ottavio42 <ottaviog88@gmail.com>
2021-09-08 13:03:23 +0200

    fine-es4

---
+++ snack4/js/script.js
@@ -0,0 +1,19 @@
+function fondere(array1,array2){
+    let result=[],max;
+    if(array2.length<array1.length){
+        max=array1.length;
+    }else{
+        max=array2.length;
+    }
+    for(let i=0; i<max;i++){
+        if(array1[i]!=undefined){
+            result.push(array1[i]);
+        }
+        if(array2[i]!=undefined){
+            result.push(array2[i]);
+        }
+    }
+    return result;
+}
+var array1=[1,2,3,4], array2=["a","b","c"];
+console.log fondere(array1,array2));
